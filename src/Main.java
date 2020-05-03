42978ea552114630ab2c19e9dc096e98555d0247
Alexander Pavlenko <pava@bk.ru>
2020-05-03 15:41:14 +0700

    Code review 1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         long ticket_price = 1526241; //Цена билета в копейках
-        long mile_price = 20000; //Стоимость мили в копейках
+        long mile_price = 2000; //Стоимость мили в копейках
         long miles_count = ticket_price / mile_price; // Количество миль
         // Выводим результат
         System.out.printf("Миль начислено: %d", miles_count);
